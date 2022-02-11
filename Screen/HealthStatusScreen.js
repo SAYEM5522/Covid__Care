d28d393ec3bc80f4e2cb77e562ed612510869785
SAYEM5522 <md1040582@gmail.com>
2022-02-11 16:33:05 +0600

    HealthStatusScreen Stack Add

--- Screen/HealthStatusScreen.js
+++ Screen/HealthStatusScreen.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HealthStatus from '../Components/HealthStatusScreen/HealthStatus';
+const HealthStatusStack = createNativeStackNavigator();
 
 const HealthStatusScreen = () => {
   return (
-    <View>
-      <Text>HealthStatusScreen</Text>
-    </View>
+    <HealthStatusStack.Navigator >
+      <HealthStatusStack.Screen name="HealthStatus" component={HealthStatus} options={{headerShown:false}} />
+    </HealthStatusStack.Navigator>
   )
 }
 
