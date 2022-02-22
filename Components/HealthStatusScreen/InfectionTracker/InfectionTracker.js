b2737d5f63ae7850ef7c2d36b7eaeda0121b00be
SAYEM5522 <md1040582@gmail.com>
2022-02-22 20:26:21 +0600

    InfectionTracker Screen Add

---
+++ Components/HealthStatusScreen/InfectionTracker/InfectionTracker.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InfectionTracker = () => {
+  return (
+    <View>
+      <Text>InfectionTracker</Text>
+    </View>
+  )
+}
+
+export default InfectionTracker
+
+const styles = StyleSheet.create({})
