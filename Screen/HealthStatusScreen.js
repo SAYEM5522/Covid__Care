aff6ddd947781bb555b65e3a2774da1831dbe302
SAYEM5522 <md1040582@gmail.com>
2022-02-25 15:07:03 +0600

    Screen Folder Updated

--- Screen/HealthStatusScreen.js
+++ Screen/HealthStatusScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HealthStatus from '../Components/HealthStatusScreen/HealthStatus';
+
 const HealthStatusStack = createNativeStackNavigator();
 
 const HealthStatusScreen = () => {
