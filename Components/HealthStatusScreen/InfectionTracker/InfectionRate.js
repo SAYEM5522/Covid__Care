c918331cc874e77675de55dc2e379656c96cf74b
SAYEM5522 <md1040582@gmail.com>
2022-03-04 19:57:38 +0600

    Infection Rate Add

--- Components/HealthStatusScreen/InfectionTracker/InfectionRate.js
+++ Components/HealthStatusScreen/InfectionTracker/InfectionRate.js
@@ -1,20 +1,22 @@
-import { Dimensions, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Dimensions, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useCallback, useEffect } from 'react'
-import Svg, { Circle } from 'react-native-svg'
+import Svg, { Circle, G } from 'react-native-svg'
 import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedProps,
-  useAnimatedStyle,
-  interpolate,
   Easing,
 } from 'react-native-reanimated';
+import { useSelector } from 'react-redux';
+import { selectCount } from '../../features/InTrackerSlice';
 const BACKGROUND_COLOR = 'black';
-const BACKGROUND_STROKE_COLOR = '#303858';
+const BACKGROUND_STROKE_COLOR = '#49887B';
 const STROKE_COLOR = '#fff';
 const { width, height } = Dimensions.get('window');
-const CIRCLE_LENGTH = 1000; // 2PI*R
+const CIRCLE_LENGTH = 500; // 2PI*R
 const R = CIRCLE_LENGTH / (2 * Math.PI);
+const HALF_WIDTH = R + 15;
+
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 const styles = StyleSheet.create({
@@ -38,50 +40,90 @@ const styles = StyleSheet.create({
     backgroundColor: BACKGROUND_COLOR,
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  TextAnimation:{
+    fontSize: R / 3,
+    fontWeight: "500",
+    textAlign: "center",
+    textShadowColor: "white",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 4,
+    elevation: 3,
+    color:"white",
+    ...StyleSheet.absoluteFill
+  }
 
 })
+const clamp = (x ,min, max) => {
+  "worklet";
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
+};
 const InfectionRate = () => {
   const progress = useSharedValue(0);
+  const t=useSelector(selectCount)
+  const inputRef = React.useRef();
+  const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
   const animatedProps = useAnimatedProps(() => ({
-    strokeDashoffset:CIRCLE_LENGTH * (1 - progress.value),
+    strokeDashoffset: CIRCLE_LENGTH * (1 - progress.value),
   }));
-  const onPress = useCallback(() => {
-    'worklet'
-    progress.value = withTiming(progress.value=0.7,{ duration: 250 });
-  }, [])
-  const AnimatedStyle=useAnimatedStyle(()=>{
-    return{
-      transform:[{
-        translateX:withTiming(interpolate(progress.value,[0,1],[width/2,0],{easing:Easing.linear,duration:1000}))
-      }]
-    }
-  })
 
+  const convert=()=>{
+    'worklet';
+   const result=(t/10)*1/0.6
+    return{
+          result
+    }
+  }
+  useEffect(()=>{
+    progress.value=withTiming(convert().result,{duration:1000,easing:Easing.linear})
+  },[])
+  const animatedInputProps = useAnimatedProps(() => {
+    const percentComplete = clamp(progress.value, 0, 1);
+
+    return {
+      text: `${Math.round(100 * percentComplete)}%`,     
+    };
+  });
   return (
     <Animated.View style={[styles.container,]}>
-        <Svg style={{ position: 'absolute' }}>
+        <Svg
+         width={R * 2}
+         height={R * 2}
+        viewBox={`${-HALF_WIDTH} ${-HALF_WIDTH} ${2 * HALF_WIDTH} ${
+          2 * HALF_WIDTH
+        }`}
+        >
+         <G rotation={"-90"} >
         <Circle
-          cx={width / 2}
-          cy={height / 2}
+          cx={0}
+          cy={0}
           r={R}
           stroke={BACKGROUND_STROKE_COLOR}
-          strokeWidth={30}
+          strokeWidth={15}
         />
+      
         <AnimatedCircle
-          cx={width / 2}
-          cy={height / 2}
+          cx={0}
+          cy={0}
           r={R}
           stroke={STROKE_COLOR}
           strokeWidth={15}
           strokeDasharray={CIRCLE_LENGTH}
           animatedProps={animatedProps}
           strokeLinecap={'round'}
+          fill="transparent"
         />
+        </G>
       </Svg>
-      <Pressable onPress={onPress} style={styles.button}>
-        <Text style={styles.buttonText}>Check</Text>
-      </Pressable>
+      <AnimatedTextInput
+          editable={false}
+          defaultValue="0%"
+          style={styles.TextAnimation}
+          animatedProps={animatedInputProps}
+        />
     </Animated.View>
   )
 }
